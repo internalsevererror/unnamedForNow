9c99ce30d15c00be3f82d61d7a1378dc642b3db7
ripi3nt <theplayer37690@gmail.com>
2025-05-17 18:59:52 +0200

    Changed network request to post

--- src/Port.ts
+++ src/Port.ts
@@ -13,20 +13,30 @@ export class Port {
   public addVessel(vessel: Vessel): void {
     //choose dock and call addVessel on that dock
     let prediction = 0;
-    fetch("localhost:5000/predict")
+    fetch("localhost:5000/predict", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        size: {
+          length: vessel.size.length,
+          width: vessel.size.width,
+          depth: vessel.size.depth,
+        },
+        name: vessel.name,
+        cargoType: vessel.cargoType,
+        expectedArrival: vessel.expectedArrival,
+        speed: vessel.speed,
+        unloadTimeframe: vessel.unloadTimeframe,
+      }),
+    })
       .then((res) => res.json())
       .then((data) => {
-        prediction = data.best_dock;
-      })
-      .catch((err) => {
-        console.log("API error");
-        console.dir(err);
-      });
-      /*if(prediction != 0) {
+        console.log(
+          "Assigned vessel " + vessel.name + " to dock " + prediction
+        );
+      }).catch(err => {console.log("API error: " + err)});
+    /*if(prediction != 0) {
         this.docks[prediction].addVessel(vessel);
       }*/
-    console.log(
-      "Assigned vessel " + vessel.name + " to dock " + prediction
-    );
   }
 }
