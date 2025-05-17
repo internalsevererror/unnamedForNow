e96d2f2cc680ceb2305c943c11e85b1c807acca5
ripi3nt <theplayer37690@gmail.com>
2025-05-17 18:48:22 +0200

    Fetch from python model

--- src/Port.ts
+++ src/Port.ts
@@ -1,18 +1,31 @@
-import {Dock} from "./Dock"
+import { Dock } from "./Dock";
 import { Vessel } from "./Vessel";
 
 export class Port {
-  public name:string;
+  public name: string;
   public docks: Dock[];
 
   constructor(name: string, docks: Dock[]) {
     this.name = name;
     this.docks = docks;
-  } 
+  }
 
   public addVessel(vessel: Vessel): void {
     //choose dock and call addVessel on that dock
-    this.docks[0].addVessel(vessel);
-    console.log("Assigned vessel " + vessel.name + " to dock " + this.docks[0].name)
+    let prediction = 0;
+    fetch("localhost:5000/predict")
+      .then((res) => res.json())
+      .then((data) => {
+        prediction = data.best_dock;
+      })
+      .catch((err) => {
+        console.log("API error");
+      });
+      /*if(prediction != 0) {
+        this.docks[prediction].addVessel(vessel);
+      }*/
+    console.log(
+      "Assigned vessel " + vessel.name + " to dock " + prediction
+    );
   }
 }
